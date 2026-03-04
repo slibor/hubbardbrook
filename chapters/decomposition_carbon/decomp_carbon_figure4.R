380272c9f3d4fe189110f2c324d7c24db1f04c75
mnwude <mnwude@vt.edu>
2026-03-04 12:06:18 -0500

    finished ch4 figure 4

--- chapters/decomposition_carbon/decomp_carbon_figure4.R
+++ chapters/decomposition_carbon/decomp_carbon_figure4.R
@@ -1,44 +1,68 @@
 library(tidyverse)
 library(plotly)
 
-SoilMassData <- read_csv("chapters/decomposition_carbon/HubbardBrook_ForestFloor_SoilMass_W6.csv")
+SoilMassData <- read_csv(
+  "https://pasta.lternet.edu/package/data/eml/knb-lter-hbr/172/3/775e243b7a2b67c0047498533bf5b9d1"
+)
 
 # basic cleaning
 SoilMassData2 <- SoilMassData |>
-  select("Year", "OM_TM", "OM_OM", "OM_LOI") |>
+  select("Year", "Plot", "OM_TM", "OM_OM", "OM_LOI") |>
   # where OM_LOI = -9999.99, other OM values are zero
   filter(OM_LOI != -9999.99)
 
-# one value for each year
 SoilMassData3 <- SoilMassData2 |>
+  group_by(Year, Plot) |>
+  summarize(OM = sum(OM_OM, na.rm = TRUE), .groups = "drop") |>
+  mutate(Mg_ha = OM * 10)
+
+# calculate SE
+st.err <- function(x) {
+  sd(x, na.rm = T) / sqrt(length(x))
+}
+
+SoilMassData4 <- SoilMassData3 |>
   group_by(Year) |>
   summarize(
-    mean_OM = mean(OM_OM),
-    min_OM = min(OM_OM),
-    max_OM = max(OM_OM)
+    OM = mean(Mg_ha, na.rm = TRUE),
+    se = st.err(Mg_ha),
+    .groups = "drop"
   )
 
 # PLOTS
-theme_set(theme_classic())
+theme_set(theme_bw())
 
-plot1 <- ggplot(SoilMassData2, aes(Year, OM_OM)) +
-  geom_point() +
-  labs(x = "Year", y = "Forest floor OM mass (Mg/ha)")
+plot1 <- ggplot(SoilMassData4, aes(Year, OM)) +
+  geom_point(size = 4) +
+  labs(x = "Year", y = "Forest floor OM mass (Mg/ha)") +
+  geom_errorbar(aes(ymin = OM - se, ymax = OM + se)) +
+  scale_y_continuous(
+    expand = c(0, 0),
+    limits = c(0, 120),
+    breaks = c(0, 20, 40, 60, 80, 100)
+  ) +
+  scale_x_continuous(expand = c(0, 0), limits = c(1970, 2020)) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    text = element_text(size = 24)
+  )
 plot1
 
-plot2 <- ggplot(SoilMassData3, aes(Year, mean_OM)) +
-  geom_point() +
-  labs(x = "Year", y = "Forest floor OM mass (Mg/ha)") +
-  geom_errorbar(aes(ymin = min_OM, ymax = max_OM))
-plot2
-
 # convert to plotly graph
-plot1_plotly <- ggplotly(plot2)
+plot1_plotly <- ggplotly(plot1) |> layout(modebar = list(
+  bgcolor = "white",
+  color = "black",
+  activecolor = "#1B5E20"
+))
 plot1_plotly
 
 htmlwidgets::saveWidget(
   widget = plot1_plotly,
-  here::here("chapters", "decomposition_carbon", "Fig4_OrganicMatter.html"),
+  here::here(
+    "chapters",
+    "decomposition_carbon",
+    "Fig4_OrganicMatter.html"
+  ),
   selfcontained = TRUE
 )
-
